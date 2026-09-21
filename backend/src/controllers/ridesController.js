7c5eb5e43aa4d07838623eecaa926b32e7121eb3
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2026-09-21 18:14:14 +0200

    Model controller rides

--- backend/src/controllers/ridesController.js
+++ backend/src/controllers/ridesController.js
@@ -1,1 +1,56 @@
 const { ridesModel } = require('../models/ridesModel');
+const { driverModel } = require('../models/driversModel');
+
+exports.createRide = async (req, res) => {
+	try {
+		const { driverId, pickup, dropoff, price } = req.body;
+        const driver = await driverModel.findOne({
+			_id: driverId,
+			status: 'available'
+		});
+        if (!driver) {
+			return res.status(409).json({ error: 'Driver non disponibile' });
+		}
+        const ride = await ridesModel.create({
+			passengerId: req.user.user_Id,
+			driverId,
+			pickup: pickup,
+			dropoff: dropoff,
+			price
+		});
+        const createdRide = await populateRide(ridesModel.findById(ride._id));
+		return res.status(201).json(createdRide);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+exports.getRideById = async (req, res) => {
+	try {
+		const { id } = req.params;
+        const ride = await populateRide(ridesModel.findById(id));
+        if (!ride) {
+			return res.status(404).json({ error: 'Corsa non trovata' });
+		}
+		return res.status(200).json(ride);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+exports.updateRideStatus = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { status } = req.body;
+		const ride = await ridesModel.findById(id);
+		if (!ride) {
+			return res.status(404).json({ error: 'Corsa non trovata' });
+		}
+		ride.status = status;
+		await ride.save();
+		const updatedRide = await populateRide(ridesModel.findById(ride._id));
+		return res.status(200).json(updatedRide);;
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
--- backend/src/models/ridesModel.js
+++ backend/src/models/ridesModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const RideSchema = new mongoose.Schema({
     passengerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    pickupLocation: { type: String, required: true },
-    dropoffLocation: { type: String, required: true },
+    driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', required: true},
+    pickup: { type: String, required: true },
+    dropoff: { type: String, required: true },
     status: { type: String, enum: ['pending', 'accepted', 'in_progress', 'completed', 'cancelled'], default: 'pending'},
     price: { type: Number, required: true },
     dateTime: { type: Date, required: true, default: Date.now }
